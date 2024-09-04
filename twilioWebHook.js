d65d7988dfacd952836888f590ea5e6a0bbbfbb6
Al Watkins <alwatkins@Als-MacBook-Pro.local>
2024-09-04 18:09:09 -0400

    Initial Commit

---
+++ twilioWebHook.js
@@ -0,0 +1,49 @@
+import  nodemailer  from 'nodemailer';
+import { validateRequest } from 'twilio';
+
+//Twilio Credentials
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+//Nodemailer Transporter Configuation
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+
+export default async function handler(req, res) {
+    if (req.method === 'POST'){
+        const twilioSignature = req.headers['x-twilio-signature'];
+        const url = '';
+        const params = req.body;
+    }
+
+    const isValidRequest = twilio.validRequest(twilioAuthToken, twilioSignature, url, params);
+    
+    if (!isValidRequest) {
+        return res.status(403).send('Invalid request signature');
+    }
+
+    const from = req.body.From;
+    const body = req.body.Body;
+
+    const mailOptions = {
+        from: process.env.GMAIL_USER,
+        to: 'hello@atpeacearts.com',
+        subject: `New text from ${from}`,
+        text: `You have a new SMS from ${from}: \n\n ${body}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({message: 'Email sent succesfully.'});
+    }catch (error) {
+        console.error('Error sending email', error);
+        res.status(500).json({message: 'Failed to send email'});
+    }
+}
+
+
+
